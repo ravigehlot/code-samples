5ad7d793eea10688a873a3b5df0815fdc38953c8
Ravi Gehlot <ravi@gehlot.net>
2022-05-14 11:31:57 -0400

    Added Code Samples for PowerShell ForEach-Object

---
+++ ForEach-Object.ps1
@@ -0,0 +1,33 @@
+﻿<#
+    ForEach-Object
+#>
+
+# Create a JSON string
+$json = '{
+    "names": [
+        {
+            "firstName": "Ravi", 
+            "lastName": "Gehlot"
+        },
+        {
+            "firstName": "Eva", 
+            "lastName": "Gehlot"
+        }
+    ]
+}';
+
+# Convert the JSON string into an object
+$obj = Write-Output $json | ConvertFrom-Json;
+
+# Display the object type
+$type = $obj.getType();
+
+# Loop on the object displaying the properties
+if( $type.BaseType.Name -eq "Object") {
+    Write-Host "Looping through the object properties";
+
+    ForEach-Object {
+        Write-Host $obj.names.firstName
+    }    
+}
+
